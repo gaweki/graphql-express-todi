5fc6deb21e6cc3d6d236b570f267ffa998ff1c92
andreltokdis <andrel@tokodistributor.com>
2020-08-24 21:08:11 +0700

    feat: Add playground

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const {GraphQLSchema} = require('graphql');
 
-const {queryType} = require('./query.js');
+const { queryType } = require('./query.js');
+const expressPlayground = require("graphql-playground-middleware-express").default;
 
 //setting up the port number and express app
 const port = 4500;
@@ -21,6 +22,9 @@ app.use('/graphql', graphqlHTTP({
     schema: schema,
     graphiql: true,
 }));
+
+//Graphql Playground route
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
 
 app.listen(port);
 console.log(`GraphQL Server Running at localhost:${port}`);
